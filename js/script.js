5365543a2365a01081f89c45088d5aba3d4bf281
Eliana-Abate <eliana.abate@outlook.it>
2021-07-29 15:48:43 +0200

    added array taskList

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ console.log ('collegato', Vue);
 const root = new Vue ({
     el: '#root',
     data: {
-        
+        taskList = [
+            'Organizzare il viaggio',
+            'Cambiare data del volo',
+            'Fare la valigia',
+            'Caricare batteria della macchina fotografica',
+            'Fare la spesa',
+        ],
     }
 });
